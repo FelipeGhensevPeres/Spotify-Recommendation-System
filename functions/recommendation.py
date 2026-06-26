f9a3830dc989abf8dcd3ebd8519669b4d602069c
Felipe Ghensev Peres <felipeghensev@gmail.com>
2026-06-26 11:37:00 -0300

    chore: adiciona comentarios sobre a priorização de generos musicais e quantidade flexivel de musicas ao buscar

--- functions/recommendation.py
+++ functions/recommendation.py
@@ -3,7 +3,11 @@ import pandas as pd
 def recommend_by_index(indice,
                     df_modelagem_escalado,
                     modelo_NN,
-                    df_spotify):
+                    df_spotify,
+                    qtd_musicas_buscar=6):
+    
+    if qtd_musicas_buscar < 1:
+        raise ValueError
     
     musica = df_modelagem_escalado[indice]
     
@@ -11,12 +15,15 @@ def recommend_by_index(indice,
     
     musica = musica.reshape(1,-1)
     
-    distancias, vizinhos = modelo_NN.kneighbors(musica)
+    distancias, vizinhos = modelo_NN.kneighbors(musica,n_neighbors=qtd_musicas_buscar)
     
     df_recommendation_music = df_spotify.iloc[vizinhos[0]][["track_name", "artists", "album_name", "track_genre"]]
 
     condicao_genero = df_recommendation_music["track_genre"].str.lower() == genero_musical.lower()
     
+    # Prioriza recomendações do mesmo genero musical.
+    # Caso não exista recomendações suficientes, musicas de outros generos.
+    # Também são mantidas para não reduzir a quantidade de resultados.
     mesmo_genero = df_recommendation_music[condicao_genero]
     outros_generos = df_recommendation_music[~condicao_genero]
 
